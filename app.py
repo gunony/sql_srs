013664292d6b94ac01171574d7f3ed94cc8906c2
Guillaume Nony <gunony@gmail.com>
2025-06-05 22:12:14 +0200

    correction du commit

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ df=pd.DataFrame(data)
 st.write("REQUETE SQL !") #imprime titre
 
 tab1, tab2 = st.tabs(["REQUETE" ,"dog"]) #defini les tab avec leur noms
+
+with tab1:
     sql_query = st.text_area(label="entrez votre requête")
     st.write(f"la requête demandée est : {sql_query}")
     result = duckdb.query(sql_query).df() #resultat du sql converti en df
